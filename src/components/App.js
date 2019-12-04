cbca81780fc81ba01255ae933e1c92b393daf470
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-03 21:37:04 -0800

    added multiple goals comps as filler content

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Greeting from './greeting/Greeting';
+import Goals from './goals/Goals';
 
 export default function App() {
   return (
-    <>
+    <main>
       <h1>Hello World</h1>
       <Greeting />
-    </>
+      <Goals />
+      <Goals />
+      <Goals />
+    </main>
   );  
 }
