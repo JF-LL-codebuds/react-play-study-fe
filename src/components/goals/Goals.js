432dd92e0b5147f95cbb138a08ccdd71709b328b
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-03 21:36:37 -0800

    created goals comp

---
+++ src/components/goals/Goals.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './goals.css';
+
+function Goals() {
+  return (
+    <section className={styles.goals}>
+      <h2>Goals for Here</h2>
+      <ul>
+        <li>have user sign in</li>
+        <li>use react router</li>
+        <li>drop down to select comps in 1 place</li>
+        <li>other stuff as thunkened</li>
+        <li>PRACTICE MOBILE!!!</li>
+        <li>Modular Object Display Comp</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Goals;
