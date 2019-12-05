a3f1b2a95d4e3e94f89d66b1c394324c5bef5ac3
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-04 18:53:11 -0800

    making some dummy data

---
+++ src/services/data/pokemon.js
@@ -0,0 +1,39 @@
+let pokeData = [
+  {
+    _id: '000001',
+    name: 'short bread',
+    pokemon: 'snorlax',
+    type: ['normal'],
+    image: 'none',
+  },
+  {
+    _id: '000002',
+    name: 'winder',
+    pokemon: 'gloom',
+    type: ['grass', 'poison'],
+    image: 'none',
+  },
+  {
+    _id: '000003',
+    name: 'fire brains',
+    pokemon: 'haunter',
+    type: ['ghost'],
+    image: 'none',
+  },
+  {
+    _id: '000004',
+    name: '',
+    pokemon: 'bulbasaur',
+    type: ['grass'],
+    image: 'none',
+  },
+  {
+    _id: '000005',
+    name: 'beardy-o',
+    pokemon: 'gyrados',
+    type: ['water', 'dragon'],
+    image: 'none',
+  },
+];
+
+export default pokeData;
