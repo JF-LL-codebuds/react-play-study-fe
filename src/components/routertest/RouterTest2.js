5f34a685173a16628779a55b08d0f66473c77edd
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-07 17:12:01 -0800

    extra filler comps to test react router changes

---
+++ src/components/routertest/RouterTest2.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Goals from '../goals/Goals';
+import goalStyle from '../goals/goals.css';
+
+export default function RouterTest1() {
+  return (
+    <>
+      <Goals sectionStyle={goalStyle.goals3} />
+      <Goals sectionStyle={goalStyle.goals3} />
+      <Goals sectionStyle={goalStyle.goals3} />
+      <Goals sectionStyle={goalStyle.goals3} />
+      <Goals sectionStyle={goalStyle.goals3} />
+      <Goals sectionStyle={goalStyle.goals3} />
+      <Goals sectionStyle={goalStyle.goals3} />
+      <Goals sectionStyle={goalStyle.goals3} />
+      <Goals sectionStyle={goalStyle.goals3} />
+    </>
+  );
+}
