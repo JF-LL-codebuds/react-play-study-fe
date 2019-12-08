928156ab190344c20ea7bdc712be01986809e8b6
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-07 17:10:33 -0800

    refactored app to use react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Home from './home/Home';
+import RouterTest1 from './routertest/RouterTest1';
+import RouterTest2 from './routertest/RouterTest2';
 import Greeting from './greeting/Greeting';
 import Goals from './goals/Goals';
 import PokeComp from './dynamicobj/PokeComp';
@@ -15,21 +23,34 @@ export default function App() {
   return (
     <main>
       <h1 className={styles.topopage}>Time for the Learny-Learn</h1>
-      <Greeting />
-      <section className={styles.goalsSection}>
-        <Goals sectionStyle={goalStyles.goals1} />
-        <Goals sectionStyle={goalStyles.goals4} />
 
-        <PokeComp pokemon={pokeData[1]} />
 
-        <Goals sectionStyle={goalStyles.goals2} />
-        <Goals sectionStyle={goalStyles.goals3} />
+      <Router>
+        <Greeting />
+        <section className={styles.goalsSection}>
+          <>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/routertest1" component={RouterTest1} />
+              <Route path="/routertest2" component={RouterTest2} />
+            </Switch>
+          </>
+        </section>
+      </Router>
 
-        <PokeComp pokemon={pokeData[2]} />
-        <PokeComp pokemon={pokeData[0]} />
-        <PokeComp pokemon={pokeData[3]} />
-        <PokeComp pokemon={pokeData[4]} />
-      </section>
     </main>
   );  
 }
+
+{/* <Goals sectionStyle={goalStyles.goals1} />
+<Goals sectionStyle={goalStyles.goals4} />
+
+<PokeComp pokemon={pokeData[1]} />
+
+<Goals sectionStyle={goalStyles.goals2} />
+<Goals sectionStyle={goalStyles.goals3} />
+
+<PokeComp pokemon={pokeData[2]} />
+<PokeComp pokemon={pokeData[0]} />
+<PokeComp pokemon={pokeData[3]} />
+<PokeComp pokemon={pokeData[4]} /> */}
