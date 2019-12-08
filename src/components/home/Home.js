f2038c4c514f0c2bc9b41d06baa04f924853a1bf
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-07 17:11:24 -0800

    made new home comp from app to work w/react router

---
+++ src/components/home/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Goals from '../goals/Goals';
+import PokeComp from '../dynamicobj/PokeComp';
+import goalStyles from '../goals/goals.css';
+import pokeData from '../../services/data/pokemon';
+
+export default function Home() {
+  return (
+    <>
+      <Goals sectionStyle={goalStyles.goals1} />
+      <Goals sectionStyle={goalStyles.goals4} />
+
+      <PokeComp pokemon={pokeData[1]} />
+
+      <Goals sectionStyle={goalStyles.goals2} />
+      <Goals sectionStyle={goalStyles.goals3} />
+
+      <PokeComp pokemon={pokeData[2]} />
+      <PokeComp pokemon={pokeData[0]} />
+      <PokeComp pokemon={pokeData[3]} />
+      <PokeComp pokemon={pokeData[4]} />
+    </>
+
+  );
+
+}
