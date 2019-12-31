cec70d14c22e55685abfc689397d9661fe74c443
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-30 22:38:11 -0800

    have rudementary fight action in engine

--- src/services/jrpgengine/jrpgEngine.js
+++ src/services/jrpgengine/jrpgEngine.js
@@ -1,9 +1,21 @@
 const CombatEngine = {
   player: {
-    fight: function(enemyAC, playerToHit) {
+    fight: function(enemyAC, playerHitBonus) {
+      let fightActionReturnObject = {};
       console.log('enemy AC', enemyAC);
-      console.log('player hit bonus', playerToHit);
-      return CombatEngine.dieRolls.d20() + 'here we go';
+      console.log('player hit bonus', playerHitBonus);
+      let thisRoll = CombatEngine.dieRolls.d20() + playerHitBonus;
+      console.log('you rolled a ' + thisRoll);
+      if(thisRoll >= enemyAC) {
+        fightActionReturnObject.combatMsg = 'you hit the enemy';
+        let damageRoll = CombatEngine.dieRolls.d8();
+        fightActionReturnObject.damage = damageRoll;
+      } 
+      else {
+        fightActionReturnObject.combatMsg = 'you missed the enemy';
+        fightActionReturnObject.damage = 0; 
+      }
+      return fightActionReturnObject;
     },
 
   },
