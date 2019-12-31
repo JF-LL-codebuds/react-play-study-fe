f98fc6d54577b9950907caf1f749d31d15815e14
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-30 19:06:44 -0800

    trying out a very basic action

--- src/components/gamelayout/GameLayout.js
+++ src/components/gamelayout/GameLayout.js
@@ -3,13 +3,15 @@ import Actions from './Actions';
 import MainScreen from './MainScreen';
 import YourStats from './YourStats';
 import EnemyComp from './EnemyComp';
+import CombatEngine from '../../services/jrpgengine/jrpgEngine';
 import enemies from '../../services/gamedata/enemies';
+import players from '../../services/gamedata/players';
 import styles from './gamelayout.css';
 
 class GameLayout extends Component {
   state = {
     currentEnemy: 'none',
-    player: {},
+    player: players[0],
     battlesFought: 0,
   };
 
@@ -21,6 +23,16 @@ class GameLayout extends Component {
       return this.setState({ currentEnemy: enemies[randomEnemy] });
     }
   }
+
+  //player actions linked from engine here
+  playerTriesToHit = () => {
+    let tempReturnVal = CombatEngine.player.fight(this.state.currentEnemy.armorClass,
+      this.state.player.hitBonus);
+    console.log(tempReturnVal);
+  }
+
+
+
 
   componentDidMount() {
     /* eslint-disable-next-line no-console */
@@ -34,7 +46,7 @@ class GameLayout extends Component {
       <div className={styles.containerStyle}>
         <MainScreen className={styles.mainScreen}/>
         <div>
-          <Actions />
+          <Actions playerTriesToHit={this.playerTriesToHit}/>
           <YourStats />
           <EnemyComp enemy={this.state.currentEnemy}/>
         </div>
