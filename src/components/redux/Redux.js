9c600d64943866625b0276d4f8217bf3743b0874
Jonathan Forney <jonathan.forney@gmail.com>
2020-02-05 21:41:54 -0800

    this file still needs lots of work

---
+++ src/components/redux/Redux.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { createStore } from 'redux';
+
+const store = createStore(counter);
+
+
+class Redux extends Component {
+
+
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+
+export default Redux;
